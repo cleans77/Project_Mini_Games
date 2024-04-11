56fa663dd63da6410eda8820d8f548fe04822ab5
cleans77 <sdh230202@sdh.hs.kr>
2024-04-11 11:20:41 +0900

    vs.0.02.02.1
    M_Battle Script Update

--- Unity_MiniGame/Assets/Undertale_Battle/Script/Undertale_GameManager.cs
+++ Unity_MiniGame/Assets/Undertale_Battle/Script/Undertale_GameManager.cs
@@ -10,6 +10,17 @@ public class Undertale_GameManager : MonoBehaviour
 {
     Attack_action Act;                  //공격 방식 선정 코드
     public Transform[] Spawn_Position = null;  //공격 스폰 위치
+    [SerializeField] private GameObject Bone = null;        //기본 공격 프리팹
+    [SerializeField] private GameObject GarstBlade = null;  //가스터 블레이드 프리팹
 
+    [SerializeField] private Transform Ground_Point = null;    //땅 위치값
+    [SerializeField] private Transform Ceiling_Point = null;   //천장 위치값
+
+    [SerializeField]
     Vector2[] NotePos;                  //기억 안남 ㅋ
+
+    public void Transofqokdfodfbopfjengwds(string adada)
+    {
+
+    }
 }
